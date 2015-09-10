9a48ec5067df0885b86190c6771a8e1c7fbb458e
tomlinton <tlinton@fastmail.fm>
2015-09-11 09:54:28 +1000

    Add setup.py contents

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(name='flask_cheddargetter',
+      version='0.1.0',
+      description='A Flask extension for integrating subscriptions via'
+                  'CheddarGetter (http://cheddargetter.com).',
+      author='Thomas Linton',
+      author_email='tlinton@fastmail.fm',
+      packages=['flask_cheddargetter'],
+      requires=['lxml', 'requests', 'inflection']
+)
+
